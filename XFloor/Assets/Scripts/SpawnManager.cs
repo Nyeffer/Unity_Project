de1a2666adbcc61fed55b4a58382b0766a246fb0
Jeffherson Vega <jeffhersonlim@gmail.com>
2017-11-01 21:24:03 -0400

    Add One more Platform

--- XFloor/Assets/Scripts/SpawnManager.cs
+++ XFloor/Assets/Scripts/SpawnManager.cs
@@ -3,45 +3,43 @@ using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
-    private GameObject platform;
-    public GameObject platform_FlatGrass;
-    public GameObject platform_SlopeGrass;
-    public Transform platform_0;
-    public Transform platform_N1;
-    public Transform platform_P1;
-    public Transform platform_N2;
-    public Transform platform_P2;
-    public Transform platform_N3;
-    public Transform platform_P3;
-    public Transform platform_N4;
-    public Transform platform_P4;
+    public GameObject[] platform;
+    private int num_platform;
+
+    public Transform[] spawnPoints;
+
+    private int num_SpawnPoints;
+
+
+    
 
     // Use this for initialization
     void Start()
     {
-        int rand = Random.Range(1,3);
-        switch(rand) {
-            case 1:
-                platform = platform_FlatGrass;
-                break;
-            case 2: 
-                platform = platform_SlopeGrass;
-                break;
-            default:
-                break;
-        }
         Spawn();
     }
     void Spawn()
     {
-            Instantiate(platform, platform_0.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_N1.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_N2.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_N3.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_N4.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_P1.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_P2.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_P3.transform.position, Quaternion.identity);
-            Instantiate(platform, platform_P4.transform.position, Quaternion.identity);
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            int rand = Random.Range(1, 4);
+            PickAPlatform(i, rand);
+        }
+    }
+
+    private void PickAPlatform(int i, int rand)
+    {
+        switch (rand)
+        {
+            case 1:
+                Instantiate(platform[0], spawnPoints[i].position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(platform[1], spawnPoints[i].position, Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(platform[2], spawnPoints[i].position, Quaternion.identity);
+                break;
+        }
     }
 }
